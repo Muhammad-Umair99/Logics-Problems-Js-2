a3dff0c387e271f98a96bf19b1a9c61e3c72ba36
Muhammad-Umair99 <muhammadumair10010@gmail.com>
2024-09-10 22:42:29 +0500

    some more logics

--- main.js
+++ main.js
@@ -163,4 +163,107 @@
 // console.log(res2);
 
 
+// 11) Object Methods
+// • Question: Create an object calculator with methods add, subtract, multiply, and divide. Each
+// method should take two numbers as arguments and return the result of the corresponding
+// operation.
+
+
+// In programming, an "object" is a collection of properties and methods. When we refer to a "calculator object," we're talking about a specific instance of an object designed to perform calculations. Here’s a more detailed breakdown:
+
+// Concept of an Object
+// In many programming languages, especially those that support object-oriented programming, an object is a way to group related data and functions together. An object consists of:
+
+// 1) Properties: Variables that hold data related to the object. 
+// 2) Methods: Functions that perform actions or computations related to the object
+
+
+// const calculator = {
+//     add: function (a, b) {
+//         return a + b
+//     },
+//     subtract: function (a, b) {
+//         return a - b
+//     },
+//     multiply: function (a, b) {
+//         return a * b
+//     },
+//     divide: function (a, b) {
+//         if (b === 0) {
+//             throw new Error(`Division by zero is not allowed`);
+//         }
+//         return a / b
+//     }
+// }
+// console.log(calculator.add(5, 3));  
+// console.log(calculator.subtract(5, 3));  
+// console.log(calculator.multiply(5, 3));  
+// console.log(calculator.divide(5, 5));  
+
+
+
+//  ES6 method shorthand syntax:
+// const calculator = {
+//   // Method for addition
+//   add(a, b) {
+//     return a + b;
+//   },
+
+
+//   subtract(a, b) {
+//     return a - b;
+//   },
+
+
+//   multiply(a, b) {
+//     return a * b;
+//   },
+
+
+//   divide(a, b) {
+//     if (b === 0) {
+//       throw new Error('Division by zero is not allowed.');
+//     }
+//     return a / b;
+//   }
+// };
+
+// console.log(calculator.add(5, 3));  
+// console.log(calculator.subtract(5, 3));  
+// console.log(calculator.multiply(5, 3));  
+// console.log(calculator.divide(5, 5)); 
+
+
+//12) Nested If-Else
+// • Question: Write a function that checks a student's grade and returns "A" for marks above 90,
+// "B" for marks between 80 and 90, "C" for marks between 70 and 80, and "F" for marks below
+// 70.
+
+
+// let checkGrade = (marks) => {
+//     if (marks > 90) {
+//         return "A";
+//     } else if (marks >= 80) {
+//         return "B";
+//     } else if (marks >= 70) {
+//         return "C";
+//     } else {
+//         return "F";
+//     }
+// };
+
+// let result = checkGrade(55);
+// console.log(result); 
+
+
+
+// 13) Array Length
+// • Question: How do you find the length of an array in JavaScript?
+
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+// console.log(arr.length);
+
+// arr.pop();
+// console.log(arr.length);
+
 
