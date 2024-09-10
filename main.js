34f15e69f40d91dd7bc58c572561f1bc8c725430
Muhammad-Umair99 <muhammadumair10010@gmail.com>
2024-09-10 17:25:33 +0500

    more js logics

--- main.js
+++ main.js
@@ -53,7 +53,7 @@
 // 5) If-Else Statement
 // • Question: Write a function that takes a number as an argument and returns "Positive" if the
 // number is greater than 0, "Negative" if the number is less than 0, and "Zero" if the number is  0.
- 
+
 
 // first)
 // function integrs (numbers){
@@ -62,15 +62,15 @@
 //     }
 //     else if(numbers > 0){
 //         console.log("Positive");
-    
+
 //     }
 //     else{
 //         console.log("Zero")
 //     }
 //     
 // }
-    // let res = integrs(0)
-    // console.log(res);
+// let res = integrs(0)
+// console.log(res);
 
 
 
@@ -91,8 +91,76 @@
 
 
 
+// 6) While Loop
+// • Question: Write a while loop that prints numbers from 1 to 5.
+
+// let num = 1;
+// while(num <= 5){
+//     console.log(num);
+//     num++;
+// };
 
 
+
+// 7) Math Operations
+// • Question: Write a function that calculates the square root of a number using JavaScript's
+// Math object.
+
+// function squareRoot(number) {
+//     return Math.sqrt(number);
+// }
+
+// let result = squareRoot(5);
+// console.log(result);
+
+
+
+//8)
+// calculates the square of the number. 
+
+// function square(number) {
+//     return number * number;
+// }
+// let res = square(5);
+// console.log(res);
+
+
+
+
+// 9) Array Filtering
+// • Question: Write a function that takes an array of numbers and returns a new array
+// containing only the even numbers.
+
+// function evenNumbers(arr) {
+//     let result = [];               // Initialize an empty array to store even numbers
+//     for (let val of arr) {
+//         if (val % 2 === 0) {
+//             result.push(val);      // Add even number to the result array
+//         }
+//     }
+//     return result
+// };
+
+// let res = evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+// console.log(res);
+
+
+
+
+// 10) Function with Default Parameters
+// • Question: Create a function that multiplies two numbers. If the second number is not
+// provided, it should default to 10.
+
+
+// const multiply = ( x , y = 10)=>{
+//    return x * y
+// };
+
+// res1 = multiply(5,5);
+// console.log(res1);
+
+// res2 = multiply(5);
+// console.log(res2);
 
 
 
