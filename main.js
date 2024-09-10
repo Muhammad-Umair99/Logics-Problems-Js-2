ba1617e6c397fe48587ba4f7cb3597edc21b285e
Muhammad-Umair99 <muhammadumair10010@gmail.com>
2024-09-10 15:55:18 +0500

    js logics

---
+++ main.js
@@ -0,0 +1,98 @@
+//  1) Object Property Access
+// • Question: Create an object named car with properties make, model, and year. How would
+// you access and print the model of the car?
+
+// const car = {
+//     name : "Toyota",
+//     model : "Toyota Supra",
+//     year: 2024,
+// };
+// console.log(car.model);
+
+
+// 2) Array Iteration
+// • Question: Given an array of numbers, write a for loop that prints each number in the array.
+
+// let arr = [1,2,3,4,5,6,7];
+// console.log(arr);
+// for(let i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+
+// 3) Array Sum Function
+// • Question: Write a function that takes an array of numbers as an argument and returns the
+// sum of all elements in the array.
+
+
+// function numbers(arr){
+//     let sum = 0
+//     for(let val of arr){
+//     sum = sum + val
+//     }
+//     return sum
+//  }
+
+//  let result = numbers([1,2,3,4,5,6]);
+//  console.log(result);
+
+
+// 4) Date Manipulation
+// • Question: How would you create a new Date object in JavaScript and extract the current year
+// from it?
+
+// let date = new Date();
+
+// console.log(date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() );
+
+// console.log(date.getFullYear());
+
+
+
+
+// 5) If-Else Statement
+// • Question: Write a function that takes a number as an argument and returns "Positive" if the
+// number is greater than 0, "Negative" if the number is less than 0, and "Zero" if the number is  0.
+ 
+
+// first)
+// function integrs (numbers){
+//     if(numbers < 0){
+//         console.log("Negative");
+//     }
+//     else if(numbers > 0){
+//         console.log("Positive");
+    
+//     }
+//     else{
+//         console.log("Zero")
+//     }
+//     
+// }
+    // let res = integrs(0)
+    // console.log(res);
+
+
+
+
+//second)
+// function integrs(number) {
+//     if (number < 0) {
+//         return "Negative";
+//     } else if (number > 0) {
+//         return "Positive";
+//     } else {
+//         return "Zero";
+//     }
+// }
+
+// let res = integrs(5); 
+// console.log(res); 
+
+
+
+
+
+
+
+
